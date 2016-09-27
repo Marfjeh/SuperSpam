e466d20263c66d9a2d8ad9b768b53f00a4e0d4f2
Marvin Ferwerda <Marfjeh@users.noreply.github.com>
2016-09-27 12:36:40 +0200

    Update Form1.cs

--- SuperSpam/Form1.cs
+++ SuperSpam/Form1.cs
@@ -3,6 +3,15 @@
  * And how the hell did you got the source code? o_O
  * Marf is not a real C# programmer, he has made this in his own time because his friends needed a program like this. 
  * Do not hit Marf because of this. Marf has done no harm to you.
+ *
+ *
+ * Copryight MarfProjects 2016
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, 
+ * INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, 
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. 
+ * IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, 
+ * DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, 
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
  * */
 
 using System;
